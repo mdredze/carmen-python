99b1763aa23b4a970eef8113c63c0b463eb47882
Mark Dredze <mdredze@cs.jhu.edu>
2017-01-10 12:45:23 -0500

    Updated version and authors

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='carmen',
-    version='0.0.3',
+    version='0.0.4',
     description='Geolocation for Twitter',
-    author='Roger Que',
-    author_email='query@jhu.edu',
-    url='https://github.com/query/carmen',
+    author='Roger Que and Mark Dredze',
+    author_email='mark@dredze.com',
+    url='https://github.com/mdredze/carmen-python',
     packages=find_packages(),
     package_data={'carmen': ['data/*']},
     install_requires=['geopy'],
