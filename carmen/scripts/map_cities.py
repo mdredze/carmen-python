6ffb4b1179516a1343f3d8e9afbd989f78989ec5
Hugh Han <hughhan1@gmail.com>
2017-03-08 12:45:03 -0500

    Added Comments to map_cities.py on How it Works

--- carmen/scripts/map_cities.py
+++ carmen/scripts/map_cities.py
@@ -6,11 +6,20 @@ import sys
 # Connect to the database
 conn = psycopg2.connect("dbname='carmen' user='hhan' password=''")
 
+# This is the name of the Carmen data file
+carmen_filename = '../data/locations.json'
+
+
+def write_output(carmen_id, geonames_id, city, region, country_code):
+    sys.stdout.write(
+        "%s -> %d    (%s, %s, %s)\n" 
+        % (carmen_id, geonames_id, city, region, country_code)
+    )
+
 
 def main():
     cur = conn.cursor()
 
-    carmen_filename = '../data/locations.json'
     carmen_data = open(carmen_filename)
 
     num_success           = 0  # these numbers are used to track
@@ -84,42 +93,40 @@ def main():
             # if we STILL can't find it, then we will say it doesn't exist in Geonames.
             if (x == None):
 
-                latitude  = data['latitude']
-                longitude = data['longitude']
-
-                cur.execute(
-                    "SELECT id, alternatenames FROM cities WHERE "
-                    "latitude  >= %s AND "
-                    "latitude  <  %s AND "
-                    "longitude >= %s AND "
-                    "longitude <  %s AND "
-                    "alternatenames IS NOT NULL AND "
+                latitude  = data['latitude']                # If we couldn't find the name
+                longitude = data['longitude']               # of our city in Geonames, then
+                                                            # perhaps we should try checking
+                cur.execute(                                # the alternate names.
+                    "SELECT id, alternatenames "
+                    "FROM cities WHERE "                    # The 'aliases' attribute in the
+                    "latitude  >= %s AND "                  # Carmen data isn't too great, so
+                    "latitude  <  %s AND "                  # we will instead check Geonames'
+                    "longitude >= %s AND "                  # 'alternatenames' field, which
+                    "longitude <  %s AND "                  # returns a strings separated by
+                    "alternatenames IS NOT NULL AND "       # commas.
                     "country_code = %s;",
-                    (
-                        str(float(latitude)-1),
-                        str(float(latitude)+1), 
-                        str(float(longitude)-1), 
-                        str(float(longitude)+1),
+                    (                                       # We will query only on nearby
+                        str(float(latitude)-1),             # locations, of +/- 1 degree of
+                        str(float(latitude)+1),             # latitude/longitude, with the
+                        str(float(longitude)-1),            # same country code as the 
+                        str(float(longitude)+1),            # city we are searching for.
                         country_code 
                     )
                 )
                 possible_locations = cur.fetchall()
 
-                name_found = False
-                for t in possible_locations:
-                    alternatenames = t[1].split(',')
-                    for n in alternatenames:
-                        if city == n:
+                name_found = False                          # Now we iterate over all of the
+                for t in possible_locations:                # possible locations, and try to
+                    alternatenames = t[1].split(',')        # find a match between our city
+                    for n in alternatenames:                # name and one of the many
+                        if city == n:                       # alternate names from Geonames
                             geonames_id = t[0]
-                            name_found = True
-                    if name_found:
-                        break
+                            name_found = True               # If we find the name, then we
+                    if name_found:                          # can write our output. Otherwise
+                        break                               # we give up and print an error.
                 
                 if name_found:
-                    sys.stdout.write(
-                        "%s -> %d    (%s, %s, %s)\n" 
-                        % (carmen_id, geonames_id, city, region, country_code)
-                    )
+                    write_output(carmen_id, geonames_id, city, region, country_code)
                     num_success += 1
                 else:
                     sys.stderr.write(
@@ -129,10 +136,7 @@ def main():
                     nonexistent_names += 1
             else:
                 geonames_id = x[0]
-                sys.stdout.write(
-                    "%s -> %d    (%s, %s, %s)\n" 
-                    % (carmen_id, geonames_id, city, region, country_code)
-                )
+                write_output(carmen_id, geonames_id, city, region, country_code)
                 num_success += 1
 
     carmen_data.close()
