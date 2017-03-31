b9dde2c9b1a396890dedc136164eafdf76cc9c17
Hugh Han <hughhan1@gmail.com>
2017-03-31 13:04:50 -0400

    JSON Mapping Uses Geonames Parent IDs
    The mapping now uses Geonames' hierarchy.txt instead of the original
    locations.json's parent_id field when mapping. That is, the parent_id
    field is now ignored in the mapping, and the actual parents are
    instead looked up in hierarchy.txt.
    
    Another small update was updating the
    carmen/mappings/regions_errors.txt file.

--- carmen/scripts/rewrite_json.py
+++ carmen/scripts/rewrite_json.py
@@ -2,18 +2,21 @@ import json
 import sys
 
 
-carmen_filename  = '../data/locations.json'
-new_filename     = '../data/new.json'
-cities_filename  = '../mappings/cities.txt'
-regions_filename = '../mappings/regions.txt'
+carmen_filename    = '../data/locations.json'
+new_filename       = '../data/new.json'
+cities_filename    = '../mappings/cities.txt'
+regions_filename   = '../mappings/regions.txt'
+
+hierarchy_filename = '../data/dump/hierarchy.txt'
 
 
-def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping):
+def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping, parents):
     old_file = open(old_filename, "r")
     new_file = open(new_filename, "w")
 
-    num_success = 0
-    num_errors  = 0
+    num_success         = 0
+    num_errors          = 0
+    num_missing_parents = 0
     for line in old_file:
         data      = json.loads(line)   # get the data from the JSON file, and
         carmen_id = data['id']         # retrieve its carmen ID and its parent
@@ -28,7 +31,7 @@ def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping):
             except KeyError:
                 num_errors += 1
                 sys.stderr.write(
-                    "%s (%s, %s, %s) is not a valid Geonames ID\n" 
+                    "%s (%s, %s, %s) does not map to a Geonames ID\n" 
                     % (data['id'], data['city'], data['state'], data['country'])
                 )
                 continue
@@ -42,17 +45,17 @@ def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping):
                 num_errors += 1
                 if ( data['county'] != '' ):
                     sys.stderr.write(
-                        "%s (%s, %s) is not a valid Geonames ID\n" 
+                        "%s (%s, %s) does not map to Geonames ID\n" 
                         % (data['id'], data['county'], data['country'])
                     )
                 elif ( data['state'] != '' ):
                     sys.stderr.write(
-                        "%s (%s, %s) is not a valid Geonames ID\n" 
+                        "%s (%s, %s) does not map to a Geonames ID\n" 
                         % (data['id'], data['state'], data['country'])
                     )
                 else:
                     sys.stderr.write(
-                        "%s (%s) is not a valid Geonames ID\n" 
+                        "%s (%s) does not map to a Geonames ID\n" 
                         % (data['id'], data['country'])
                     )
                 continue
@@ -61,11 +64,31 @@ def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping):
             if data['parent_id'] == "-1":
                 pass
             else:
-                data['parent_id'] = regions_mapping[parent_id] # their Geonames IDs
+                # data['parent_id'] = regions_mapping[parent_id]
+                data['parent_id'] = parents[data['id']]
         except KeyError:
-            num_errors += 1
-            sys.stderr.write("parent ID " + data['parent_id'] + " is not a valid Geonames ID\n")
-            continue
+            num_missing_parents += 1
+            if data['city'] != '':
+                sys.stderr.write(
+                    "%s (%s, %s, %s) does not have a Geonames parent ID\n"
+                    % (data['id'], data['city'], data['state'], data['country'])
+                )
+            elif data['county'] != '':
+                sys.stderr.write(
+                    "%s (%s, %s, %s) does not have a Geonames parent ID\n"
+                    % (data['id'], data['county'], data['state'], data['country'])
+                )
+            elif data['state'] != '':
+                sys.stderr.write(
+                    "%s (%s, %s) does not have a Geonames parent ID\n"
+                    % (data['id'], data['state'], data['country'])
+                )
+            else:
+                sys.stderr.write(
+                    "%s (%s) does not have a Geonames parent ID\n"
+                    % (data['id'], data['country'])
+                )
+            data['parent_id'] = "-1"
 
         json.dump(data, new_file)               # write result to file
         new_file.write("\n")
@@ -81,18 +104,24 @@ def rewrite_json(old_filename, new_filename, cities_mapping, regions_mapping):
 def main():
     cities_file     = open(cities_filename, "r")
     regions_file    = open(regions_filename, "r")
+    hierarchy_file  = open(hierarchy_filename, "r")
     cities_mapping  = {}
     regions_mapping = {}
+    parents         = {}
     for line in cities_file:
         components = "".join(line.split()).split("(")[0].split("->")
         cities_mapping[components[0]] = components[1]
     for line in regions_file:
         components = "".join(line.split()).split("(")[0].split("->")
         regions_mapping[components[0]] = components[1]
+    for line in hierarchy_file:
+        components = line.split()
+        parents[components[0]] = components[1]
     cities_file.close()
     regions_file.close()
+    hierarchy_file.close()
     
-    rewrite_json(carmen_filename, new_filename, cities_mapping, regions_mapping)
+    rewrite_json(carmen_filename, new_filename, cities_mapping, regions_mapping, parents)
 
 
 if __name__ == '__main__':
