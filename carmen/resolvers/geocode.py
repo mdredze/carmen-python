b676558e57d799984ed827e0606d7e363c177327
Jack Zhang <jzhan237@jhu.edu>
2021-09-30 13:52:53 -0400

    Improve coordinates retrieval for v1

--- carmen/resolvers/geocode.py
+++ carmen/resolvers/geocode.py
@@ -56,32 +56,49 @@ class GeocodeResolver(AbstractResolver):
         data = tweet.get('data')
         if data is None:
             # API v1
+            v2 = False
             tweet_coordinates = (tweet.get('coordinates') or {}).get('coordinates')
         else:
             # API v2
+            v2 = True
             geo = data.get('geo') or {}
             tweet_coordinates = (geo.get('coordinates') or {}).get('coordinates')
 
-        # Enhancement (Jack 09/15/21): another way to get coordinates is from 
-        #       includes->places->[0]->geo->bbox
-        # the bbox is a list of four coordinates. 
-        # Avg 0 and 2 to get 1st coord, and avg 1 & 3 to get the 2nd 
         if not tweet_coordinates:
-            places = tweet.get('includes', {}).get('places', None)
-            if not places:
-                return None
-            place = places[0]
-            bbox = place.get('geo', {}).get('bbox')
-            if not bbox:
-                return None
-            float_coords = [
-                (float(bbox[0])+float(bbox[2]))/2,
-                (float(bbox[1])+float(bbox[3]))/2
-            ]
-            tweet_coordinates = [
-                float(f"{float_coords[0]:.7f}"), 
-                float(f"{float_coords[1]:.7f}")
-            ]
+            if v2:
+                # Works in v2
+                # Enhancement (Jack 09/15/21): another way to get coordinates is from 
+                #       includes->places->[0]->geo->bbox
+                # the bbox is a list of four coordinates. 
+                # Avg 0 and 2 to get 1st coord, and avg 1 & 3 to get the 2nd 
+                places = tweet.get('includes', {}).get('places', None)
+                if not places:
+                    return None
+                place = places[0]
+                bbox = place.get('geo', {}).get('bbox')
+                if not bbox:
+                    return None
+                float_coords = [
+                    (float(bbox[0])+float(bbox[2]))/2,
+                    (float(bbox[1])+float(bbox[3]))/2
+                ]
+                tweet_coordinates = [
+                    float(f"{float_coords[0]:.7f}"), 
+                    float(f"{float_coords[1]:.7f}")
+                ]
+            else:
+                # v1:
+                #       place->bounding_box->coordinates->[0]->a list of lists of len 2 (long, lat)
+                coords = tweet.get('place', {}).get('bounding_box', {}).get('coordinates', None)
+                if not coords:
+                    return None
+                coords = coords[0]
+                coords = [[float(el) for el in ls] for ls in coords]
+                # list(zip(*[[1,2],[3,4],[5,6]])) -> [(1, 3, 5), (2, 4, 6)]
+                coords_transform = list(zip(*coords))
+                coords_avg = [sum(el)/len(el) for el in coords_transform]
+                assert len(coords_avg) == 2
+        if tweet_coordinates is None: return
         tweet_coordinates = Point(longitude=tweet_coordinates[0],
                                   latitude=tweet_coordinates[1])
         closest_candidate = None
