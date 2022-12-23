8be9de69563fdc6f4aa3a2ac837ce1db74a1d82b
Jack Zhang <jzhan237@jhu.edu>
2022-12-23 17:12:54 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='carmen',
-    version='0.0.4',
+    version='2.0.0',
     description='Geolocation for Twitter',
-    author='Jack Jingyu Zhang, Alexandra DeLucia, Roger Que and Mark Dredze',
+    author='Jack Jingyu Zhang, Alexandra DeLucia, Roger Que, and Mark Dredze',
     author_email='mark@dredze.com',
     url='https://github.com/mdredze/carmen-python',
     packages=find_packages(),
     package_data={'carmen': ['data/*']},
-    install_requires=['geopy', 'jsonlines'],
+    install_requires=[
+        'geopy>=1.11.0',
+        'jsonlines>=3.1.0',
+    ],
     license='2-clause BSD',
     zip_safe=True)
